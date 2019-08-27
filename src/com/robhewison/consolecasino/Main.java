c8f1bf1cbd39bba07b068bcdbe3f3c24207425c7
robhewison <robhewison1@gmail.com>
2019-08-27 14:28:20 -0400

    Create basic heads or tails game

--- src/com/robhewison/consolecasino/Main.java
+++ src/com/robhewison/consolecasino/Main.java
@@ -1,8 +1,26 @@
 package com.robhewison.consolecasino;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("Welcome to Console Casino!");
+	Scanner reader = new Scanner(System.in);
+	System.out.println("Pick a heads(1) or tails(2):");
+	int userDecision = reader.nextInt();
+	reader.close();
+
+	//Creating random generation
+    Random rand = new Random();
+    int n = rand.nextInt(2);
+    n += 1;
+
+    if (userDecision == n) {
+        System.out.println("You won!");
+    }
+    else {
+        System.out.println("You lost!");
+        }
     }
 }
